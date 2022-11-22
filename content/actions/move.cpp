dffc0fa706796a141eb2e9d1b70ba67d19c91ffd
Peter S Hermanson <peter.hermanson@gmail.com>
2022-11-21 22:12:30 -0600

    added comments

--- content/actions/move.cpp
+++ content/actions/move.cpp
@@ -12,15 +12,20 @@ Result Move::perform(Engine& engine) {
     Vec position = actor->get_position();
     Vec new_position = position + direction;
     Tile& tile = engine.dungeon.tiles(new_position);
+
+    // if not walkable and not a door, allow retry
     if (tile.is_wall() || tile.actor) {
         return failure();
     }
+
+    // if a closed door, open it
     if (tile.is_door() && !tile.walkable) {
         Door& door = engine.dungeon.doors.at(new_position);
         door.open();
         tile.walkable = true;
         return success();
     }
+
     actor->change_direction(direction);
     actor->move_to(new_position);
     return success();
