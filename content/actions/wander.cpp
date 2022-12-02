c2ff95f44dc5762d21a3f8e32d8f0efae7ea337b
Peter S Hermanson <peter.hermanson@gmail.com>
2022-12-02 14:06:14 -0600

    updated wander.cpp random function

--- content/actions/wander.cpp
+++ content/actions/wander.cpp
@@ -6,13 +6,14 @@
 
 #include "actor.h"
 #include "engine.h"
+#include "randomness.h"
 #include "tile.h"
 
 Result Wander::perform(Engine& engine) {
     Vec pos = actor->get_position();
     std::vector<Vec> neighbors = engine.dungeon.neighbors(pos);
     // randomize directions
-    std::random_shuffle(std::begin(neighbors), std::end(neighbors));
+    shuffle(std::begin(neighbors), std::end(neighbors));
     for (const Vec& neighbor : neighbors) {
         Tile& tile = engine.dungeon.tiles(neighbor);
         if (!tile.is_wall() && !tile.actor) {
